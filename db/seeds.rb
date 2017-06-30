d12aba5853feeed4849ea20161d129795d9ab879
Ngo Van Dinh <ngovandinht2@gmail.com>
2017-06-30 10:27:39 +0700

    seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create ! name: admin, email: 'admin@gmail.com', password: '123456', password_confirmation: '123456'
+User.create! name: 'admin', email: 'admin@gmail.com', password: '123456', password_confirmation: '123456'
